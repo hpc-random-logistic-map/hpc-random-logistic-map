efe08ca440396c6c2c35bfa0b5a68910cf3b0f01
Amy Le <anle9371@colorado.edu>
2014-12-03 17:32:16 -0700

    now this generates the appropriate shell files.

--- C_code/generate_cmdlines.cpp
+++ C_code/generate_cmdlines.cpp
@@ -20,52 +20,76 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    int maxarg = 13;
-    if(argc != maxarg){
-	cout << argc << " arguments. Incorrect inputs. Try something like:\n ./generate_cmdlines.exe -L 0.1 -r 3.2 -iter 1000 -h 0.01 -f cmdlines -ab myrand.csv";
+  int maxarg = 11;
+  if(argc != maxarg){
+    printf("Used %d arguments. Expected %d arguments. Try something like:\n ./generate_cmdlines.exe -L 0.1 -dr 0.1 -dx 0.1 -iter 1000 -f result.csv",argc,maxarg);
+  }
+  else{
+    double L, r, x, dr, dx;
+    int tmp, numx, numr, iter, nc, nm, nd;
+    char mydata[100], mybuf[300];   
+    ofstream ofs, ofsdriver;
+    char cmdlines[50], myrand[50], driver[50];
+    string sjob, resfile;
+    string c;
+
+    for (int i = 1; i < maxarg; i+=2){
+      c = argv[i];
+      if(c == "-L"){
+	L = atof(argv[i+1]);   // sim param L in [0,1]
+      }
+      if(c == "-dr"){
+	dr = atof(argv[i+1]);    
+      }
+      if(c == "-iter"){
+	iter = atoi(argv[i+1]);
+      }
+      if(c == "-dx"){
+	dx = atof(argv[i+1]);
+      }
+      if(c == "-f"){
+	resfile = argv[i+1];
+      }
     }
-    else{
-	double L, r, x, h;
-	int tmp, N, iter;
-	char mydata[500];   
-	ofstream ofs;
-	string mfn, ab;
-	string c;
 
-	for (int i = 1; i < maxarg; i+=2){
-	    c = argv[i];
-	    if(c == "-L"){
-		L = atof(argv[i+1]);   // sim param L in [0,1]
-	    }
-	    if(c == "-r"){
-		r = atof(argv[i+1]);    // sim param r in [0,4]
-	    }
-	    if(c == "-iter"){
-		iter = atoi(argv[i+1]);
-	    }
-	    if(c == "-h"){
-		h = atof(argv[i+1]);
-	    }
-	    if(c == "-f"){
-		mfn = argv[i+1];
-	    }
-	    if(c == "-ab"){
-	        ab = argv[i+1];
-	    }
-	}
-	N = 1/h;
-	x = 0.0;
-	
-	ofs.open(mfn.c_str(), ofstream::out | ofstream::trunc);
-	for( int i = 0; i < N; i++){
-	    x = x + h;
-	    tmp = sprintf(mydata, "./myfunc.exe -L %f -r %f -x0 %f -iter %d -f %s \n",L,r,x,iter,ab.c_str());  //write data to buffer: mydata
-	    ofs << mydata;    //write mydata to file mfn
-	}
+    numr = ((4-dr) - (0+dr))/dr;
+    numx = ((1-dx) - (0+dx))/dx;
+    r = 0.0;
 
-	ofs.close();
+    for(int j = 0; j < numr; j++){
+      x = 0.0;
+      r = r + dr;
+      nc = sprintf(cmdlines,"cmdline_%d",j);
+      nm = sprintf(myrand, "myrand_%d.csv",j);
+      nd = sprintf(driver, "driver_%d.sh",j);
 
-	return 0;
+      //generate shell scripts driver_j.sh      
+      ofsdriver.open(driver, ofstream::out | ofstream::trunc);
+      tmp = sprintf(mybuf,"#!/bin/bash\n#SBATCH -N 1\n#SBATCH --ntasks-per-node=12\n#SBATCH --time=01:00:00\n#SBATCH --job-name=driver_%d\n#SBATCH --output=driver_%d.out\n./generate_rands.exe -L %f -r %f -f myrand_%d.csv\ntime mpirun load_balance -f cmdline_%d -s dynamic\n",j,j,L,r,j,j);
+      ofsdriver << mybuf; 
+      ofsdriver.close();
 
+      //generate cmdline_j
+      ofs.open(cmdlines, ofstream::out | ofstream::trunc);
+      for( int i = 0; i < numx; i++){
+	x = x + dx;
+	tmp = sprintf(mydata, "./myfunc.exe -L %f -r %f -x0 %f -iter %d -f %s >> %s\n",L,r,x,iter,myrand,resfile.c_str());  //write data to buffer: mydata
+	ofs << mydata;    //write mydata to file 
+      }
+      ofs.close();
+    }    
+
+    //generate submitjob.sh
+    sjob = "submitjob.sh";
+    ofs.open(sjob.c_str(), ofstream::out | ofstream::trunc);
+    ofs << "chmod 777 driver_*\n";
+    for(int j = 0; j < numr; j++){
+      tmp = sprintf(mydata, "sbatch --qos=janus driver_%d.sh\n", j);
+      ofs << mydata;
     }
+    ofs.close();
+
+    return 0;
+
+  }
 }
