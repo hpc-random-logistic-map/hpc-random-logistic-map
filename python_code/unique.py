550ad2c0a855c5ef28a43e0685c7ebcbd9319d16
Kristina Entzel <kren7615@login02.rc.colorado.edu>
2014-12-06 13:03:52 -0700

    uniqeness

--- python_code/unique.py
+++ python_code/unique.py
@@ -10,24 +10,19 @@ import pdb
 csvf = open(sys.argv[1], 'r')
 
 csvreader = csv.reader(csvf)
-
+data =[]
 #csvreader.next() # skip first row
 #pdb.set_trace()
 for index,row in enumerate(csvreader):
     #print index
-    flag = False
-    csvf1 = open(sys.argv[1], 'r')
-    csvreader1 = csv.reader(csvf1)
-    for index1,row1 in enumerate(csvreader1):
-        #print index1
-        #print row1
-        if index1== index:
-            break
-        if row1[2] == row[2]:
-            if row[3:] == row1[3:]:
-                  flag = True
-                  break
-    csvf1.close()
-    if flag == False:
-        print row[3:]                                 
+	flag = False
+	if(index != 0):
+		for i in data:
+		#	print i
+			if(row == i):
+				flag = True
+				break
+        if (flag == False):
+		data.append(row)                         
 csvf.close()
+print data
